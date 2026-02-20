b3a52e73caa1ad8a35865d698434833196ea0cbb
Gaurav Singh Yadav <137716439+EGAVSIV@users.noreply.github.com>
2026-02-20 19:15:21 +0530

    Update WGAT.py

--- WGAT.py
+++ WGAT.py
@@ -7,7 +7,7 @@ import os
 
 st.set_page_config(page_title="Master Scanner", layout="wide",page_icon="🟢")
 
-st.title("📈 Wave Going Against The Tide/ Swing- Momentum Scans")
+st.title("📈 Wave Going Against The Tide + SW & MOM Scans")
 
 
 def set_bg_image(image_path: str):
